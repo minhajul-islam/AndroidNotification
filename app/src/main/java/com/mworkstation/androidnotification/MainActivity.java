e2e8c81e177a6a9111ff737838e9dbea61f86564
md minhajul islam <minhajul.sust.com>
2019-07-30 01:56:03 +0600

    notification forgt=round

--- app/src/main/java/com/mworkstation/androidnotification/MainActivity.java
+++ app/src/main/java/com/mworkstation/androidnotification/MainActivity.java
@@ -1,13 +1,35 @@
 package com.mworkstation.androidnotification;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "m_notification";
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
+
+        FirebaseApp.initializeApp(context);
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String token = instanceIdResult.getToken();
+                Log.e("notification", "Refreshed token: " + token);
+            }
+        });
+
+
     }
 }
